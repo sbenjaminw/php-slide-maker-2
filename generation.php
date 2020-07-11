f21e9a65a7c983c5e212bc256327ba0713909417
sbenjaminw <59287885+sbenjaminw@users.noreply.github.com>
2020-07-11 17:55:09 +0100

    Update generation.php
    Removed old comments.

--- generation.php
+++ generation.php
@@ -13,9 +13,6 @@
 	$textcontent = $_POST["txtContents"];
 	$bg = $_POST["txtTitleSlide"];
 	
-	//var_dump($_FILES["upldFiles"]);
-	//echo "sent!";
-		
 	//	Instance of render engine.
 	$re = new RenderEngine(
 		$_FILES["upldFiles"],
@@ -24,83 +21,5 @@
 	);
 		
 	$re->MakeSVG();
-	
-		
-
-
-/*
-	if(isset($_POST["txtContents"])){
-		
-		if(isset($_POST["upldFiles"])){
-			
-			$validflag = CheckValidFileTypes();
-			//$strip_files = StripFiles();
-			
-			if($validflag == true){
-				
-				if(strlen($_POST["txtContents"]) > 0){
-					
-					//	Split the text into sentances.
-					$sentances = preg_split('/\r\n|[\r\n]/', $_POST["txtContents"]);
-									
-					$re = new RenderEngine($sentances, $_POST["upldFiles"], $_POST["txtTitleSlide"]);
-					$re->MakeSVG();
-					
-				}
-				
-			}
-			
-			
-		}
-		
-	} else {
-		
-		header("location:http://localhost/projects/automating-svg/index.html");
-		
-	}	
-	
-	//	Checks for valid file types.
-	function CheckValidFileTypes(){
-		
-		//	The end flag. By default is false.
-		$valid_files = false;
-		
-		//	Allowed file types.
-		$allowed_types = array("png", "gif", "jpg");
-			
-		//	For each uploaded file, check the path extention. If it is in the 
-		//	array, then change valid flag to true. otherwise, break.
-		foreach($_POST["upldFiles"] as $file){
-			
-			$ext = pathinfo($file, PATHINFO_EXTENSION);
-			
-			if (!in_array($ext, $allowed_types)) {
-				
-				break;
-			
-			} else {
-				
-				$valid_files = true;
-				
-			}
-			
-		}
-		
-		//	If the valid flag is true, then generate the files. 
-		//	If not, redirect.
-		if($valid_files == true){
-			
-			return true;
-			
-		} else {
-			
-			header("location:http://localhost/projects/automating-svg/index.html");
-			
-		}
-	
-	}
-	
-
-*/
 	
 ?>
